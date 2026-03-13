0c86a39ff09d26743f193e17fbce345bbbf3eefb
mathieuMarthy <mathieu.marthy@gmail.com>
2026-03-13 22:24:46 +0100

    add tauri

---
+++ bonsize-tauri/src-tauri/src/lib.rs
@@ -0,0 +1,20 @@
+use std::path::PathBuf;
+
+use bonsize_core::scanner::{ScanOptions, file_model::FileModel, get_directory_size};
+
+#[tauri::command]
+fn scan_directory(path: String) -> FileModel {
+    let path_buf = PathBuf::from(path);
+
+    let options = ScanOptions { quiet: true };
+    get_directory_size(&path_buf, &options)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![scan_directory])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ bonsize-tauri/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api/core';
+import { ref } from 'vue';
+
+const path = ref("");
+const result = ref({});
+
+async function scan_directory(path: string) {
+  result.value = await invoke("scan_directory", { path: path });
+}
+</script>
+
+<template>
+  <main>
+    <p>Salut !</p>
+
+    <label>Path</label>
+    <input v-model="path" />
+
+    <button @click="() => scan_directory(path)">scan</button>
+
+    <span>{{ result }}</span>
+  </main>
+</template>
