b394f0525ead17f588300be1bfd1534f4b77a38f
mathieuMarthy <mathieu.marthy@gmail.com>
2026-03-14 13:54:03 +0100

    add eslint and tailwind

--- bonsize-tauri/src/App.vue
+++ bonsize-tauri/src/App.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api/core';
-import { ref } from 'vue';
 
-const path = ref("");
-const result = ref({});
-
-async function scan_directory(path: string) {
-  result.value = await invoke("scan_directory", { path: path });
-}
 </script>
 
 <template>
-  <main>
-    <p>Salut !</p>
-
-    <label>Path</label>
-    <input v-model="path" />
-
-    <button @click="() => scan_directory(path)">scan</button>
-
-    <span>{{ result }}</span>
-  </main>
+    <main>
+    
+    </main>
 </template>
