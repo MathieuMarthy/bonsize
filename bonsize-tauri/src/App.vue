fbd7232aa21e2614458cdf427659a3367c88ba94
mathieuMarthy <mathieu.marthy@gmail.com>
2026-03-14 14:30:19 +0100

    home page

--- bonsize-tauri/src/App.vue
+++ bonsize-tauri/src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import MultiButton from "./components/atoms/multi-button.vue";
 
 </script>
 
 <template>
-    <main>
-    
+    <main class="w-full h-screen flex flex-col justify-around bg-background">
+        <div class="flex flex-col items-center">
+            <img class="w-46 h-auto" src="./assets/icon.png" alt="bonsize's logo" />
+            <p class="text-text text-center text-6xl font-bold">Bonsize</p>
+        </div>
+
+        <div class="flex flex-col items-center gap-4">
+            <MultiButton class="w-64" text="Pick folder"/>
+            <MultiButton class="w-64" text="Scan all my files" :full="false"/>
+        </div>
     </main>
 </template>
